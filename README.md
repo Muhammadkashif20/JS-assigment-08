4ac4a25c60b86111402368a3c89bc648882ba71f
muhammadkashif20 <kashif28@gmail.com>
2024-03-13 22:47:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-assigment-08
+ JS-assigment-chapter-if else & else if & testing sets
---
+++ app.js
@@ -0,0 +1,140 @@
+// Question :- 01
+
+var A='uppercase letter', Z='uppercase letter', a='lowercase letter', z='lowercase letter';
+if(A=='uppercase letter'){
+    alert('This is uppercase letter')
+}
+else if(Z=='uppercase letter'){
+    alert('This is uppercase letter')
+}
+else if(a=='lowercase letter'){
+    alert('This is lowercase letter')
+}
+else if(z=='lowercase letter'){
+    alert('This is lowercase letter')
+}
+
+// Question :- 02
+
+var a=10
+var b=10
+
+if(a==10 || b==10){
+    alert('two integers are equal.')
+}
+else{
+    alert('two integers  are not equal.')
+}
+
+// Question :- 03
+  
+var inputNumber= prompt('enter your Number')
+
+if(inputNumber%2==0){
+alert('this is postivie number')
+}
+else if(inputNumber%1==0){
+alert('this is negative number')
+}
+
+// Question :- 04
+var vowels= prompt('enter your vowels Alphabets')
+var a;
+var e;
+var i;
+var o;
+var u;
+if(vowels=='a'){
+    alert('this is vowels alphabets:  a')
+}
+else if(vowels=='e'){
+    alert('this is vowels alphabets:  e')
+    
+}
+else if(vowels=='i'){
+    alert('this is vowels alphabets:  i')
+    
+}
+else if(vowels=='o'){
+    alert('this is vowels alphabets:  o')
+    
+}
+else if(vowels=='u'){
+    alert('this is vowels alphabets:  u')
+    
+}
+else{
+    alert('please enter vowels alphabets')
+}
+
+// Question :- 05
+
+
+var password1= prompt('enter password1')
+var password2= prompt('enter password2')
+if(password1==123){
+    alert('correct password1')
+    if(password2==123){
+        alert('correct password2')
+    }
+  
+}
+else{
+    alert('Incorrect password')
+}
+
+// Question :- 06
+
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+
+}
+else{
+    greeting = "Good evening";
+    }
+
+// Question :- 07
+
+var inputTime=prompt('enter your input Time')
+
+if(inputTime==2400){
+    alert('this is 12:00 clock')
+}
+else if(inputTime==2300){
+    alert('this is 11:00 clock')
+
+}
+else if(inputTime==2200){
+    alert('this is 10:00 clock')
+
+}
+else if(inputTime==2100){
+    alert('this is 9:00 clock')
+
+}
+else if(inputTime==2000){
+    alert('this is 8:00 clock')
+
+}
+else if(inputTime==1900){
+    alert('this is 7:00 clock')
+
+}
+else{
+    alert('time is wrong')
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
